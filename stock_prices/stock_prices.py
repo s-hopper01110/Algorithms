afc41ddacc1e4ebeb966dd4392bb627a47ad6290
Sheila <s.hopper01110@gmail.com>
2019-05-08 18:19:36 -0400

    stock_prices.py

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python
 
 import argparse
-
 def find_max_profit(prices):
+  max_profit = 0
+  min_price = prices[0]
+
+  for price in prices:
+    min_price = min(min_price, price)
+
+    compare_profit = price - min_price
+
+    max_profit = max(max_profit, compare_profit)
   
+  return max_profit
+
+
+
+
 
 
 if __name__ == '__main__':
