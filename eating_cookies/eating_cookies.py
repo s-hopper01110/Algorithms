bbb7531f6f95574a968cfa9981890447ea942b65
Sheila <s.hopper01110@gmail.com>
2019-05-09 17:34:20 -0400

    eating_cookies-1.py

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python
 
+
+# tk ex: def fib(n, cache):
+#     if n < 2:
+#         return n
+#     elif cache[n] > 0:
+#         return cache[n]
+#     cache[n] = fib(n - 1, cache) + fib(n - 2, cache)
+#     return cache[n]
+
 import sys
 
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+  if n <= 1:
+    return 1
+  elif n == 2:
+    return 2
+  else: 
+    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+  
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
